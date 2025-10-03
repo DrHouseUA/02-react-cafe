dd66f9f3ffc0dfdcbc7861e0e249b92f0ff5fa6c
Roman Kniazhyk <romanknyazhyk@gmail.com>
2025-10-03 22:08:55 +0300

    Try complete homework2 react

---
+++ src/components/VoteStats.tsx
@@ -0,0 +1,36 @@
+import type { Votes } from "../types/votes";
+import styles from "./VoteStats.module.css";
+
+interface VoteStatProps {
+  votes: Votes;
+  totalVotes: number;
+  positiveRate: number;
+}
+
+export default function VoteStats({
+  votes: { good, bad, neutral },
+  totalVotes,
+  positiveRate,
+}: VoteStatProps) {
+  return (
+    <>
+      <div className={styles.container}>
+        <p className={styles.stat}>
+          Good: <strong>{good}</strong>
+        </p>
+        <p className={styles.stat}>
+          Neutral: <strong>{neutral}</strong>
+        </p>
+        <p className={styles.stat}>
+          Bad: <strong>{bad}</strong>
+        </p>
+        <p className={styles.stat}>
+          Total: <strong>{totalVotes}</strong>
+        </p>
+        <p className={styles.stat}>
+          Positive: <strong>{positiveRate}%</strong>
+        </p>
+      </div>
+    </>
+  );
+}
